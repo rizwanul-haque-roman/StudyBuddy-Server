40f8ba9ee2ce17dc9d75f15d5e3178b7378a80e1
Rijwanul Haque Roman <rizwanul210058@diit.edu.bd>
2024-05-12 18:28:05 +0600

    implemented pagination in the assignment page

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_USER, process.env.DB_PASS);
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uzy5irc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,15 +21,32 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
+
+    const database = client.db("studyBuddy");
+    const assignments = database.collection("assignments");
+
+    app.get("/assignments", async (req, res) => {
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      console.log(page, size);
+      const result = await assignments
+        .find()
+        .skip(page * size)
+        .limit(size)
+        .toArray();
+      res.send(result);
+    });
+
+    app.get("/totalAssignments", async (req, res) => {
+      const count = await assignments.estimatedDocumentCount();
+      res.send({ count });
+    });
   } finally {
-    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
